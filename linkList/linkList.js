1496ed31349c626681bcad2a19dbc0ad4e16e287
zhouxm <varnbu@gmail.com>
2019-01-15 15:19:41 +0800

    添加链表的插入

--- linkList/linkList.js
+++ linkList/linkList.js
@@ -24,9 +24,50 @@ function linkList() {
         length++
     }
     this.insert = function (position, element) {
-
+        if (position > -1 && position < length) {
+            let node = new Node(element)
+            let index = 0
+            let curr = head
+            let prev
+            if (position === 0) {
+                head = node
+                node.next = curr
+                head = node
+            } else {
+                while (index++ < position) {
+                    prev = curr
+                    curr = curr.next
+                }
+                prev.next = node
+                node.next = curr
+            }
+            length++
+            return true
+        } else {
+            return false
+        }
     }
-    this.removeAt = function (position) { }
+    this.removeAt = function (position) {
+        //  检查是否越界
+        if (position > -1 && position < length) {
+            let index = 0
+            let current = head.next
+            let previous
+            if (position === 0) {
+                head = current.next
+            } else {
+                while (index++ < position) {
+                    previous = current
+                    current = current.next
+                }
+                previous.next = current.next
+            }
+            length--
+            return current.element
+        } else {
+            return null
+        }
+    }
     this.remove = function (element) { }
     this.indexOf = function (element) { }
     this.size = function () { }
