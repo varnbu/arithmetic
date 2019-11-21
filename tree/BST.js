7a08a8c3eea2b4ea2cd0a025efbac52554cf7fd0
varn <vanrbu@gmail.com>
2019-11-21 18:08:43 +0800

    tree

---
+++ tree/BST.js
@@ -0,0 +1,19 @@
+class Node {
+    constructor (key) {
+        this.key = key
+        this.left = null
+        this.right = null
+    }
+}
+
+/**
+ * 二叉搜索树是
+ * 只允许左侧节点的值比父节点小，右侧节点的值比父节点大
+ */
+class BinarySearchTree {
+    root = null
+    constructor (){
+    
+    }
+}
+
